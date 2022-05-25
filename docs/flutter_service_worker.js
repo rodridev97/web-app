32e849c673e8a7d1c89c2f1c48f9c74508d86f97
rodri.dev97 <rodri.dev97@gmail.com>
2022-05-25 07:45:28 -0600

    Add Changes

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0c83187e50e372b8b3caaf7eadf5ed00",
-"/": "0c83187e50e372b8b3caaf7eadf5ed00",
+"index.html": "77ce2545303a2c732517d1cd3949ffdf",
+"/": "77ce2545303a2c732517d1cd3949ffdf",
 "main.dart.js": "531633f5cd32f601baa5bcb07928d2c1",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"
